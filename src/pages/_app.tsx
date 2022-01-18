358934b1297e5158025efad335f37c5181409eb5
M Fauzan A <fadzim018@gmail.com>
2022-01-18 18:09:13 +0700

    chore: add tailwind & styledcomponent

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
+import '../styles/globals.css';
+
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { GlobalStyles } from 'twin.macro';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <title>NextJs</title>
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    './src/pages/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backgroundColor(theme) {
+      return {
+        ...theme('colors'),
+        primary: '#FCAD10',
+      };
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
